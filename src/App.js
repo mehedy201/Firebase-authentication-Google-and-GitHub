119f31ab22a07fb1d69411a62f3dd144e6be3727
mehedy201 <mehedy643@gmail.com>
2022-04-23 22:36:34 +0600

    added home page in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>i will explore firebase authentication</h1>
+      <Home></Home>
     </div>
   );
 }
